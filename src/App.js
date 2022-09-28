9ed06b44c2b9f950997aef0125ec7e77965179b6
ASUS K 46 CM <ermansahputra.com>
2022-09-28 18:25:50 +0700

    tugas react-mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, { useState } from 'react';
 import './App.css';
+import ContactForm from './components/ContactForm';
+import Header from './components/Header';
+
+import {Card, CardContent, Container, Grid} from '@mui/material';
+import Contact from './components/Contact';
 
 // Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from './data/contacts.json';
 const App = () => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
@@ -10,8 +16,30 @@ const App = () => {
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
 
+  const handleAddContact = (value) => {
+    setContacts([...contacts, value])
+  }
+
+  const [contacts, setContacts] = useState(contactsJSON);
   return (
     <div className="App">
+      <Header />
+      <Container fixed>
+      <Grid container>
+        <Grid item md={6}>
+          <ContactForm fnAddContact={ (value) => handleAddContact(value) }/>
+        </Grid>
+        <Grid item md={6}>
+          <Card>
+            <CardContent sx={{backgroundColor: '#DBF6F0'}}>
+              {contacts.map((contact, index) => (
+                <Contact key={index} data={contact}/>
+              ))}
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      </Container>
     </div>
   );
 };
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,13 +2,93 @@
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
-
-const ContactForm = () => {
+import React, { useState } from 'react';
+import {
+    Card,
+    CardActions,
+    CardContent,
+    Button,
+    TextField,
+    FormControl,
+} from '@mui/material';
+// import Contact from './Contact';
+// import contactsJSON from '../data/contacts.json';
+const ContactForm = ({fnAddContact}) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const initialState = {
+        name:'',
+        phone:'',
+        email:'',
+        photo:'',
+    };
+
+    const [newContact, setNewContact] = useState(initialState);
+
+    const handleChange = (name, value) => {
+        setNewContact({...newContact, [name] : value})
+    }
+
+    const hendleSubmit = (e) => {
+        e.preventDefault();
+        fnAddContact(newContact);
+        setNewContact(initialState)
+    }
 
     return (
-        <> </>
+        <>
+        <Card
+          sx={{ maxWidth: 500, margin: 'auto', backgroundColor: '#f0f0f0' }}
+        >
+          <form onSubmit={hendleSubmit}>
+            <CardContent>
+              <FormControl fullWidth margin='normal'>
+                <TextField
+                  required
+                  id='outlined-required'
+                  label='Name'
+                  name='name'
+                  value={newContact.name}
+                  onChange={(e) => handleChange('name', e.target.value)}
+                />
+              </FormControl>
+              <FormControl fullWidth margin='normal'>
+                <TextField
+                  required
+                  id='outlined-required'
+                  label='Phone'
+                  name='phone'
+                  value={newContact.phone}
+                  onChange={(e) => handleChange('phone', e.target.value)}
+                />
+              </FormControl>
+              <FormControl fullWidth margin='normal'>
+                <TextField
+                  required
+                  id='outlined-required'
+                  label='Email'
+                  name='email'
+                  value={newContact.email}
+                  onChange={(e) => handleChange('email', e.target.value)}
+                />
+              </FormControl>
+              <FormControl fullWidth margin='normal'>
+                <TextField
+                  required
+                  id='outlined-required'
+                  label='Photo Url'
+                  name='photo'
+                  value={newContact.photo}
+                  onChange={(e) => handleChange('photo', e.target.value)}
+                />
+              </FormControl>
+            </CardContent>
+            <CardActions>
+              <Button size='small' type="submit" color="success" variant="outlined">ADD NEW</Button>
+            </CardActions>
+          </form>
+        </Card>
+      </>
     );
 }
 
